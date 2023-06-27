253b07b50884259a9ee823d6230d67a1bb65a660
Parami <paramiashinsana@gmail.com>
2023-06-27 14:38:53 +0530

    Create the Login

---
+++ src/main/java/controllers/LoginFormController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginFormController {
+
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.chat_applicationinpcoursework {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.chat_applicationinpcoursework to javafx.fxml;
-    exports com.example.chat_applicationinpcoursework;
-}
